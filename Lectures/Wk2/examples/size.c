78dec0c2e21aee9a2fb233edf6d5e2e9cb64628e
Brenton Schulz <brenton@z2dsp.com>
2019-02-24 08:18:23 +1100

    Added Wk2 lecture content

---
+++ Lectures/Wk2/examples/size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	char a;
+	short b;
+	int c;
+	long d;
+	printf("char: %d\n", sizeof(a));
+	printf("short: %d\n", sizeof(b));
+	printf("int: %d\n", sizeof(c));
+	printf("long: %d\n", sizeof(d));
+	return 0;
+}
