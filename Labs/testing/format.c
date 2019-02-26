1b340f6db211e0f6c595f9e4a6d6345f2aaed7b9
Brenton Schulz <brenton.schulz@newcastle.edu.au>
2019-02-26 13:34:02 +1100

    Started Week 2 lab

---
+++ Labs/testing/format.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+	char a = 1,b = 2,c = 3;
+	printf("%d\n", b);
+	return 0;
+}
--- Lectures/Wk2/examples/factors.c
+++ Lectures/Wk2/examples/factors.c
@@ -7,7 +7,7 @@ int main() {
 	x = input;
 	while(x > 0) {
 		if(input % x == 0) // ie: if the remainder is zero
-			printf("%d is a factor of %d\n", x, input);
+			printf("%d is a factor of %d", x, input);
 		x--;
 	}
 	return 0;
