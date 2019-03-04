979f5934db35c4d80b66e7ae16080949d5603a64
Brenton Schulz <brenton@z2dsp.com>
2019-03-05 05:43:31 +1100

    Moved several wk2 Tuesday slides to Friday

--- Lectures/Wk2/examples/size.c
+++ Lectures/Wk2/examples/size.c
@@ -5,9 +5,17 @@ int main() {
 	short b;
 	int c;
 	long d;
+	float f;
+	double dbl;
+	long double ldbl;
+	__float128 f128;
 	printf("char: %d\n", sizeof(a));
 	printf("short: %d\n", sizeof(b));
 	printf("int: %d\n", sizeof(c));
 	printf("long: %d\n", sizeof(d));
+	printf("float: %d\n", sizeof(f));
+	printf("double: %d\n", sizeof(dbl));
+	printf("long double: %d\n", sizeof(ldbl));
+	printf("__float128: %d\n", sizeof(f128));
 	return 0;
 }
