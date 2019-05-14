524d4b5f0d2ba693a258ebc1fe190cb4e6e8331b
Brenton Schulz <brenton.schulz@newcastle.edu.au>
2019-05-14 14:34:11 +1000

    -g typo fix

--- Lectures/Wk9/friday.m
+++ Lectures/Wk9/friday.m
@@ -1,39 +1,24 @@
+clear all
 x = imread("sunset.png");
+
 r = double(x(:,:,1));
 g = double(x(:,:,2));
 b = double(x(:,:,3));
 
-total = r+g+b;
-greyscale = total/3;
+%r = (x(:,:,1));
+%g = (x(:,:,2));
+%b = (x(:,:,3));
 
-output(:,:,1) = greyscale;
-output(:,:,2) = greyscale;
-output(:,:,3) = greyscale;
 
-map = linspace(0,1,255);
-map = [map' map' map'];
+grey = (r+g+b)/3;
 
-figure(1)
-image(r)
-axis equal
-colorbar
-colormap(map)
+greyscaleImage(:,:,1) = grey;
+greyscaleImage(:,:,2) = grey;
+greyscaleImage(:,:,3) = grey;
 
-figure(2)
-image(g)
-axis equal
-colorbar
-colormap(map)
+greyscaleImage(:,:,1) = x(:,:,1)*0.3+x(:,:,2)*0.3 + x(:,:,3)*0.3;
 
-figure(3)
-image(g)
-axis equal
-colorbar
-colormap(map)
-
-figure(4)
-image(greyscale)
-axis equal
-colorbar
-colormap(map)
-
+%greyscaleImage = greyscaleImage/255;
+%image(greyscaleImage);
+greyscaleImage = uint8(greyscaleImage);
+image(greyscaleImage);
